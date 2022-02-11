c491237fd3a3130fe1d784e469d1a6acaa1fc805
Jake Smiley <hello@jakesmiley.com>
2022-02-10 19:27:06 -0800

    functions rework and added styles

--- script.js
+++ script.js
@@ -2,40 +2,64 @@
 
 const list = [];
 
-const inputEl = document.querySelector(".list-input");
+const inputEl = document.querySelector(".input--new-task");
 const listEl = document.querySelector(".list");
 
-const addBTN = document.querySelector(".add-btn");
-const deleteBTNS = document.querySelectorAll(".delete-btn");
-
+const newTaskBtn = document.querySelector(".btn--new-task");
+const deleteBtn = document.querySelector(".btn--delete");
+const clearBtn = document.querySelector(".btn--clear");
 const checkBTN = document.querySelector(".check-btn");
 
-addBTN.addEventListener("click", function (e) {
+newTaskBtn.addEventListener("click", function (e) {
   e.preventDefault();
-  if (inputEl.value) {
-    const HTML = `
-    <li class="list-item">
-          <div class="checkbox"></div>
-
-          <div class="item-name">${input.value}</div>
-
-          <button class="btn btn--edit">
-            <ion-icon class="item-icon" name="create-outline"></ion-icon>
-          </button>
-
-          <button class="btn btn--delete">
-            <ion-icon class="item-icon" name="close-circle-outline"></ion-icon>
-          </button>
-        </li>
-      `;
-
+  const task = inputEl.value;
+  if (task) {
+    list.push(task);
+    updateListDisplay();
     inputEl.value = "";
-    listEl.insertAdjacentHTML("beforeend", HTML);
+    window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
   }
 });
 
+const updateListDisplay = function () {
+  listEl.innerHTML = "";
+  list.forEach((item) => {
+    const HTML = `
+    <li data-name="${item}"class="list-item">
+    <div class="checkbox"></div>
+    
+    <div class="item-name">${item}</div>
+    
+    <button class="btn btn--edit">
+    <ion-icon class="item-icon" name="create-outline"></ion-icon>
+    </button>
+    
+    <button class="btn btn--delete">
+    <ion-icon class="item-icon" name="trash-outline"></ion-icon>
+    </button>
+    </li>
+    `;
+
+    listEl.insertAdjacentHTML("beforeend", HTML);
+  });
+};
+
 listEl.addEventListener("click", function (e) {
-  if (e.target.classList.contains("delete-btn")) {
-    e.target.parentElement.remove();
+  console.log(e.target.closest(".item-name") || e.target.closest(".checkbox"));
+
+  //Guard clause
+  if (!e.target.closest("button")) return;
+
+  if (e.target.closest("button").classList.contains("btn--delete")) {
+    list.splice(list.indexOf(e.target.closest(".list-item").dataset.name), 1);
+    updateListDisplay();
   }
+  if (e.target.closest("button").classList.contains("btn--edit")) {
+    console.log("edit button");
+  }
+});
+
+clearBtn.addEventListener("click", function () {
+  list.length = 0;
+  updateListDisplay();
 });
